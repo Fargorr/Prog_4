6a5f988d11e97588a7c3b802cfb8f6bb50627953
Fargorr <gorodanikita@yandex.ru>
2025-10-13 18:07:47 +0700

    3 раз меняю первую задачу...

--- src/exercise1.c
+++ src/exercise1.c
@@ -7,12 +7,14 @@ int main(int argc, char** argv) {
  for(int i = 0; i < 10; ++i) {
  scanf("%lf", &array[i]);
  }
+
  int summa = 0;
  for (int i = 0; i < 10; i++) {
 	summa += (int)(array[i] * 100);
 
  }
- printf("%0.2f\n", (double)summa/1000.0);
+ summa /= 10;
+ printf("%0.2f\n", (double)summa/100.0);
  
 
  return 0;
