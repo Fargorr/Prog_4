10bb32f73e2cf328926dccac8d539909dc95f9f9
Fargorr <gorodanikita@yandex.ru>
2025-10-13 17:31:54 +0700

    Сделал второе задание

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
-    printf("Hello World!");
-    return 0;
+ double array[10];
+ 
+ for(int i = 0; i < 10; ++i) {
+ scanf("%lf", &array[i]);
+ }
+ double summa = 0;
+ for (int i = 0; i < 10; i++) {
+	summa += array[i];
+
+ }
+ printf("%0.2f\n", (double)summa/10.0);
+ 
+
+ return 0;
 }
--- src/exercise2.c
+++ src/exercise2.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
-    printf("Hello World!");
-    return 0;
+ int array[10];
+ printf("Введите десять целых чисел:\n");
+ for(int i = 0; i < 10; ++i) {
+ scanf("%d", &array[i]);
+ }
+
+ for (int i = 9; i >= 0; i--) {
+ 	printf("%d ", array[i]);
+
+
+ }
+ printf("\n");
+
+
+ return 0;
 }
+
+
