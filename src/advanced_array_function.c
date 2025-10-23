ff54d4a676cdf9c5e44b0bbd024131519dcc738a
Fargorr <gorodanikita@yandex.ru>
2025-10-24 01:13:51 +0700

    Удалил 3 задачу потому что мешало

--- src/advanced_array_function.c
+++ src/advanced_array_function.c
@@ -40,63 +40,7 @@ int length_of_lis(int* nums, int numSize) {
 }
 
 int* merge(int* intervals, int intervalsSize, int* returnSize) {
-	if (intervals == NULL) {
-		return NULL;
-	}
-	int size = intervalsSize * 2;
-	int sort[size];
-	sort[0] = intervals[0];
-	for (int i = 0; i < size - 1; i++) {
-		for (int j = 0; j < size - i - 1; j++) {
-			if (intervals[j] > intervals[j+1]) {
-				
-				sort[j] = intervals[j+1];
-				sort[j+1] = intervals[j];
-			}
-		}
-	}
-	int rezultSize = size;
-	for (int i = 1; i < size; i+=2) {
-		int start = intervals[i-1];
-		int finish = intervals[i];
-		
-		for (int j = 0; j < size-1; j++) {
-			if (sort[j] == start) {
-				for (int k = j+1; k < size-1; k++) {
-					if (sort[k] != finish) {
-						rezultSize -= 1;
-					} else if (sort[k] == finish && sort[k+1] == finish) {
-						rezultSize -= 1;
-					}
-				}
-			}
-		}	
-	}
-
-	int result_arr[rezultSize];
-	int index = 0;
-
-	for (int i = 1; i < size; i+=2) {
-                int start = intervals[i-1];
-                int finish = intervals[i];
-
-                for (int j = 0; j < size-1; j++) { 
-                        if (sort[j] == start) {
-				result_arr[index] = sort[j];
-				index += 1;
-                                for (int k = j+1; k < size-1; k++) {
-                                        if (sort[k] == finish && sort[k+1] != finish) {
-                                       		result_arr[index] = sort[k];
-						index += 1;
-						break;	
-					}
-                                }
-                        }
-                }
-        }
-	
-       	*returnSize = rezultSize / 2;
-		
+	return NULL;	
 }
 
 
