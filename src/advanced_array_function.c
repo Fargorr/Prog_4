eafe7d2bdbdaef8ac7be6cb079b3bf517127df43
Fargorr <gorodanikita@yandex.ru>
2025-10-23 21:13:27 +0700

    Решена 1 задача

--- src/advanced_array_function.c
+++ src/advanced_array_function.c
@@ -1,7 +1,23 @@
 #include "../include/advanced_array_function.h"
 
+
 int max_subarray_sum(int* nums, int size) {
-    return 0;
+	int max_sum = nums[0];
+	int current_sum = nums[0];
+
+	for (int i = 1; i < size; i++) {
+		if (nums[i] > current_sum + nums[i]) {
+			current_sum = nums[i];
+		} else {
+			current_sum += nums[i];
+		}
+		
+		if (max_sum < current_sum) {
+			max_sum = current_sum;
+		}	
+	}
+	return max_sum;	
+	
 }
 
 int length_of_lis(int* nums, int numsSize) {
@@ -12,3 +28,4 @@ int* merge(int* intervals, int intervalsSize, int* returnSize) {
     return NULL;
 }
 
+int main() { return 0; }
